2e2532acb9ce65b8aa45f7d01256a2f4fa3d743f
antoniomuchacho <antoniop2p@gmail.com>
2015-11-23 19:20:02 +0100

    aauII
    JSHISHI

---
+++ funcEdad.py
@@ -0,0 +1,15 @@
+def Edad():
+<<<<<<< Updated upstream
+  Edad = int(input("Que edad tienes?")
+  
+
+
+//porque no se actualiza?
+=======
+  Edad = input("Que edad tienes?")
+  print ("Viejo")
+
+
+
+>>>>>>> Stashed changes
+            
